557fcd80951a044e295fa130577e36e7d7138d3a
Cody Moss <codytyler.moss@gmail.com>
2014-07-01 21:44:16 -0600

    partway through

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -6,7 +6,7 @@ describe("About Functions", function() {
       return a + b;
     }
 
-    expect(add(1, 2)).toBe(FILL_ME_IN);
+    expect(add(1, 2)).toBe(3);
   });
 
   it("should know internal variables override outer variables", function () {
@@ -26,11 +26,11 @@ describe("About Functions", function() {
       return message;
     }
 
-    expect(getMessage()).toBe(FILL_ME_IN);
-    expect(overrideMessage()).toBe(FILL_ME_IN);
-    expect(message).toBe(FILL_ME_IN);
-    crazyScope();
-    expect(message).toBe(FILL_ME_IN);
+    expect(getMessage()).toBe("Outer");
+    expect(overrideMessage()).toBe("Inner");
+    expect(message).toBe("Outer");
+    crazyScope("Outer");
+    expect(message).toBe('crazy');
   });
 
   it("should have lexical scoping", function () {
--- koans/devMtnFunctions.js
+++ koans/devMtnFunctions.js
@@ -1,13 +1,21 @@
 describe('About Functions', function(){
   it('should create a function returns the current year', function(){
     //Write a function called getYear that returns the current year
+    var getYear = function() {
+          return(2014);
+        }
 
     expect(getYear()).toBe(2014);
   });
   it('should create a function returns the current year and saves it', function(){
     //Write a function that returns the current year. Then call that function and save the returned value
     //to a variable called currentYearVar
+    var currentYear = function() {
+      return 2014;
+    }     
+    var currentYearVar = currentYear() {
 
+    }
 
     expect(currentYearVar).toBe(2014);
   });
